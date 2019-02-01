c342be52ba966c91de5a393792f25d5b7fd5d6fa
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2019-02-01 14:25:31 +0300

    removed redudant log

--- src/producers/BooleanProducer.js
+++ src/producers/BooleanProducer.js
@@ -1,9 +1,6 @@
 function booleanProducer(ds, context) {
   var { _truePossibilityPercent = 50 } = ds;
   var randomPercent = Math.floor(Math.random() * 100);
-
-  console.log(_truePossibilityPercent, randomPercent);
-
   return randomPercent <= _truePossibilityPercent;
 }
 
